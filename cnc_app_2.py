f4e43a3bc088604172e537763c66cf698bd0959b
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-01-31 10:29:55 -0700

    Update cnc_app_2.py

--- cnc_app_2.py
+++ cnc_app_2.py
@@ -10,23 +10,27 @@ def load_model():
         model = pickle.load(pickled_mod)
     return model
 
+model = load_model()
+
+def prediction(input1, input2, input3, input4):
+    sl = model.predict([[input1, input2, input3, input4]])
+    return sl[0]
 
 
 #inputs - calls offered, AHT, not ready rate, total ftes, fte callouts
 
 st.text("Please fill in the responses below to predict primary care service level")
 
-model = load_model()
-
 calls_offered = st.slider(label="Choose a call volume", min_value=0, max_value=5000)
-aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5:30 is 5.5)", min_value=0, max_value=10, format="%.2f")
-not_ready = st.number_input(label="Not Ready Rate (in decimal format, i.e. 19 percent is .19)", min_value=0, max_value=1, format="%.2f")
+aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5:30 is 5.5)", min_value=4.0, max_value=7.0, step=0.1)
+not_ready = st.number_input(label="Not Ready Rate (in decimal format, i.e. 19 percent is .19)", min_value=.15, max_value=.35, step=0.01)
 total_fte = st.slider(label="Choose the total number of FTEs staffed on service line", min_value=0, max_value=65)
 call_outs = st.slider(label="Choose the estimated FTE call out equivalent", min_value=0, max_value=15)
+staffed = total_fte-call_outs
 
-feature_list = [[calls_offered, aht, not_ready, total_fte-call_outs]]
+feature_list = [[calls_offered, aht, not_ready, staffed]]
 
-sl_prediction = model.predict(feature_list)
+sl_prediction = prediction(calls_offered, aht, not_ready, staffed)
 
-st.title(f"Predicted Service Level: {int(sl_prediction)}")
+st.write(sl_prediction)
 
