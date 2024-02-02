4db1b2e023096001d035f56382d9f3d23f09e495
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-02-02 15:06:59 -0700

    Update cnc_app_2.py

--- cnc_app_2.py
+++ cnc_app_2.py
@@ -21,7 +21,7 @@ def prediction(input1, input2, input3, input4):
 
 st.text("Please fill in the responses below to predict primary care service level")
 
-calls_offered = st.number_input(label="Choose a call volume", min_value=500, max_value=3000, step=1)
+calls_offered = st.number_input(label="Enter a call volume between 500 and 3000", min_value=500, max_value=3000, step=1)
 aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.1 = 6 sec)", min_value=4.0, max_value=7.0, step=0.1)
 not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=40.0, step=0.5)
 ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=15.0, max_value=55.0, step=0.5)
